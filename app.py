f47822939d3334a0b5b58ddc2b25f7e36501031a
Mariana Martins <marianamartiyns@gmail.com>
2024-05-11 11:26:43 -0300

    update

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 import numpy as np
 
 st.header('Modelo CNN de classificação de flores')
-flower_names = ['Dente de Leao', 'Margaridas', 'Rosas', 'Girassois', 'Tulipas']
+flower_names = ['Dente de Leao','Girassois', 'Margaridas', 'Rosas','Tulipas']
 
 # Carregar o modelo completo
 try:
@@ -23,7 +23,7 @@ def classify_images(image_path):
 
         predictions = model.predict(input_image_exp_dim)
         result = tf.nn.softmax(predictions[0])
-        outcome = 'Essa imagem pertence ao grupo de ' + flower_names[np.argmax(result)] + ' com uma pontuação/precisão de '+ str(np.max(result)*100)
+        outcome = 'Essa imagem pertence ao grupo de ' + flower_names[np.argmax(result)] + ' com uma precisão de '+ str(np.max(result)*100)
         return outcome
     except Exception as e:
         return f"Erro ao classificar a imagem: {e}"
@@ -32,15 +32,16 @@ uploaded_file = st.file_uploader('Upload an Image')
 if uploaded_file is not None:
     # Salvar o arquivo enviado pelo usuário temporariamente
     try:
-        with open(os.path.join('upload', uploaded_file.name), 'wb') as f:
+        upload_dir = 'upload'
+        os.makedirs(upload_dir, exist_ok=True)  # Certificar-se de que o diretório existe
+        with open(os.path.join(upload_dir, uploaded_file.name), 'wb') as f:
             f.write(uploaded_file.getbuffer())
         
-        st.image(uploaded_file, width = 200)
+        st.image(uploaded_file, width=200)
 
         st.write("Classificação da Imagem:")
-        image_path = os.path.join('upload', uploaded_file.name)
+        image_path = os.path.join(upload_dir, uploaded_file.name)
         st.markdown(classify_images(image_path))
     except Exception as e:
         st.error(f"Erro ao processar a imagem: {e}")
-
 
