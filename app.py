c85cf6fb6cb63d5cff6bbd5a99869cff9c99a8a7
Mariana Martins <marianamartiyns@gmail.com>
2024-05-10 22:09:42 -0300

    update

---
+++ app.py
@@ -0,0 +1,46 @@
+import os
+import keras
+from keras.models import load_model
+import streamlit as st 
+import tensorflow as tf
+import numpy as np
+
+st.header('Modelo CNN de classificação de flores')
+flower_names = ['Dente de Leao', 'Margaridas', 'Rosas', 'Girassois', 'Tulipas']
+
+# Carregar o modelo completo
+try:
+    model = load_model('Flower_Recog_Model.keras')
+    st.write("Modelo carregado com sucesso!")
+except Exception as e:
+    st.error(f"Erro ao carregar o modelo: {e}")
+
+def classify_images(image_path):
+    try:
+        input_image = tf.keras.utils.load_img(image_path, target_size=(180,180))
+        input_image_array = tf.keras.utils.img_to_array(input_image)
+        input_image_exp_dim = tf.expand_dims(input_image_array,0)
+
+        predictions = model.predict(input_image_exp_dim)
+        result = tf.nn.softmax(predictions[0])
+        outcome = 'Essa imagem pertence ao grupo de ' + flower_names[np.argmax(result)] + ' com uma pontuação/precisão de '+ str(np.max(result)*100)
+        return outcome
+    except Exception as e:
+        return f"Erro ao classificar a imagem: {e}"
+
+uploaded_file = st.file_uploader('Upload an Image')
+if uploaded_file is not None:
+    # Salvar o arquivo enviado pelo usuário temporariamente
+    try:
+        with open(os.path.join('upload', uploaded_file.name), 'wb') as f:
+            f.write(uploaded_file.getbuffer())
+        
+        st.image(uploaded_file, width = 200)
+
+        st.write("Classificação da Imagem:")
+        image_path = os.path.join('upload', uploaded_file.name)
+        st.markdown(classify_images(image_path))
+    except Exception as e:
+        st.error(f"Erro ao processar a imagem: {e}")
+
+
